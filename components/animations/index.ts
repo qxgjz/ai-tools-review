cc0bd582bcea7e6b974eb622dbd232863db48e7b
qxgjz <840754587@qq.com>
2026-09-07 23:16:00 +0800

    Add 3D Creator design elements: components/animations/index.ts

---
+++ components/animations/index.ts
@@ -0,0 +1,6 @@
+export { FadeIn } from "./FadeIn";
+export { GradientText, GradientButton } from "./GradientText";
+export { Magnet } from "./Magnet";
+export { Marquee, LogoMarquee } from "./Marquee";
+export { AnimatedText } from "./AnimatedText";
+export { StackingCard, StackingCardsContainer } from "./StackingCards";
