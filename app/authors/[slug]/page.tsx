0e3893ad4da37e8e1d9b3610803cf0d14047a8c8
qxgjz <840754587@qq.com>
2026-09-12 20:43:01 +0800

    perf: add ISR revalidate=86400 to author pages

--- app/authors/[slug]/page.tsx
+++ app/authors/[slug]/page.tsx
@@ -15,6 +15,10 @@ const AUTHORS: Record<string, any> = {
   },
 };
 
+
+// ISR: revalidate author pages every 24h
+export const revalidate = 86400;
+
 export function generateStaticParams() {
   return Object.keys(AUTHORS).map((slug) => ({ slug }));
 }
