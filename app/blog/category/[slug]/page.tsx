3280f4d09e9e2241a4d44e9e40ee11137864ef78
qxgjz <840754587@qq.com>
2026-09-14 03:09:12 +0800

    Iter33: Optimize cache for app/blog/category/[slug]/page.tsx - remove force-dynamic, add ISR revalidate=3600

--- app/blog/category/[slug]/page.tsx
+++ app/blog/category/[slug]/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import posts from "@/data/posts.json";
 
-export const dynamic = "force-dynamic";
+// ISR: Cache for 1 hour, category pages rarely change
+export const revalidate = 3600;
 
 interface CategoryPageProps {
   params: { slug: string };
